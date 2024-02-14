fd9b8678853f759e3a79541f9268095d3e6b7d4b
Samdg441 <samuelgallego441@gmail.com>
2024-02-13 22:46:20 -0500

    divisiòn,multiplicacion y potencia commit

--- calculator.py
+++ calculator.py
@@ -4,3 +4,10 @@ def add(num_1, num_2):
 
 def subtract(num_1, num_2):
     return num_1 - num_2
+
+def division(num_1, num_2):
+    return num_1 / num_2
+def multiplicacion(num_1, num_2):
+    return num_1 * num_2
+def potencia(numero_1, exponente):
+    return numero_1 ** exponente
